bb280e2955444542e89d6bf53c5145558e36d0a9
David Freifeld <freifeld.david@gmail.com>
2020-03-17 12:20:51 -0700

    Added raw flux data as features.

--- archived/featuregen.py
+++ archived/featuregen.py
@@ -24,7 +24,7 @@ import pandas as pd
 import csv
 i=0
 X=[]
-y=[]
+Y=[]
 
 # Conversion from FITS files to lists.
 for root, dirs, files in os.walk("/Users/davfrei/TRAIN"):
@@ -35,37 +35,45 @@ for root, dirs, files in os.walk("/Users/davfrei/TRAIN"):
             else:
                 label=0
             X.append([])
+            Y.append([])
             hdul = fits.open("/Users/davfrei/TRAIN/"+file)
             hdul.info()
             data = hdul[1].data
+
+            for e in data["SAP_FLUX"]:
+                if np.isnan(e)!=True:
+                    Y[i].append(e)
+
+# GENERATE ANOMALY FEATURES
             for e in data["PDCSAP_FLUX"]:
                 if np.isnan(e)!=True:
                     X[i].append(e)
 
-# Usage of banpei module to find outliers, it runs a complicated stats function, but no ML involved.
-            results,results2 = detect(X[i], 0.01)
-            results,results_wide = detect(X[i], 0.2)
-            results,results_medium = detect(X[i], 0.08)
-            results,results_narrow = detect(X[i], 0.01)
-#
-# PLOTTING CODE USED FOR TESTING
-#
-#            if 1==1:
-#                plt.subplot(2, 1, 1)
-#                plt.plot(X[i])
-#                plt.title('Exoplanet Data')
-#                plt.ylabel('Luminosity')
-#                plt.subplot(2, 1, 2)
-#                plt.plot(results)
-#                plt.xlabel('Time')
-#                plt.ylabel('Anomalousness')
-#                plt.show()
-#
+# Usage of banpei module to find outliers--it runs a complicated stats function, but no ML involved.
+            results,pdc_results_wide = detect(X[i], 0.2)
+            results,pdc_results_medium = detect(X[i], 0.08)
+            results,pdc_results_narrow = detect(X[i], 0.01)
+
+            results,results_wide = detect(Y[i], 0.2)
+            results,results_medium = detect(Y[i], 0.08)
+            results,results_narrow = detect(Y[i], 0.01)
+
             i=i+1
             a=0
             past=0
             diff=[]
-            print results_narrow
+            if results_narrow != []:
+                while a < len(pdc_results_narrow):
+                    current = pdc_results_narrow[a][2]
+                    diff.append(current-past)
+                    past=current
+                    a=a+1
+                pdc_differences = stat.mean(diff)
+            else:
+                pdc_differences = -1
+            a=0
+            past=0
+            diff=[]
             if results_narrow != []:
                 while a < len(results_narrow):
                     current = results_narrow[a][2]
@@ -75,8 +83,9 @@ for root, dirs, files in os.walk("/Users/davfrei/TRAIN"):
                 differences = stat.mean(diff)
             else:
                 differences = -1
-            print differences
+
+# WRITE FEAUTURES TO FILE
 
             with open("output.csv", "a") as csv_file:
                 writer = csv.writer(csv_file, delimiter=',')
-                writer.writerow([label, len(results_wide), len(results_medium), len(results_narrow), differences])
+                writer.writerow([label, len(results_wide), len(results_medium), len(results_narrow), differences, len(pdc_results_wide), len(pdc_results_medium), len(pdc_results_narrow), pdc_differences])
